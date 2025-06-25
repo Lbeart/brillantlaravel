ad02d8336fbbfdcfceb0f49a2da5f4fb9c3751e8
Leart Bytyqi <leart.bytyqii98@gmail.com>
2025-06-25 18:04:17 +0200

    Rename Klienti model, fix class name and import

--- app/Models/Klienti.php
+++ app/Models/Klienti.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class klienti extends Model
+class Klienti extends Model
 {
     protected $table = 'klientis';
     use HasFactory;
